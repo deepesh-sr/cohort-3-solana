73f37515106873ecd429f5cc1dca88c3d682020a
deepesh-sr <220714100078@cutm.ac.in>
2025-06-17 15:59:43 +0530

    lib.rs updated

--- native-rust-sol-program-counter/src/lib.rs
+++ native-rust-sol-program-counter/src/lib.rs
@@ -1,73 +1,47 @@
-use std::ops::{AddAssign, SubAssign};
 
 use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::{
-    account_info::{AccountInfo, next_account_info},
-    entrypoint,
+    account_info::{next_account_info, AccountInfo},
     entrypoint::ProgramResult,
-    instruction::Instruction,
     msg,
     pubkey::Pubkey,
-    serialize_utils,
-    stake::instruction,
+    entrypoint
 };
 
-#[derive(BorshDeserialize, BorshSerialize)]
-enum InstructionType {
+
+#[derive(BorshSerialize, BorshDeserialize)]
+struct Counter {
+    count: u32, // 10
+}
+
+#[derive(BorshSerialize, BorshDeserialize)]
+enum CounterInstruction {
     Increment(u32),
     Decrement(u32),
 }
 
-#[derive(BorshDeserialize, BorshSerialize, Debug)]
-struct Counter {
-    count: u32,
-}
+entrypoint!(process_instruction);
 
-impl AddAssign<u32> for Counter {
-    fn add_assign(&mut self, rhs: u32) {
-        self.count += rhs;
-    }
-}
+pub fn process_instruction(
+    _program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8], // 0 , 0 0 0 1
+) -> ProgramResult { // Ok, Err(ProgramError)
+    let account = next_account_info(&mut accounts.iter())?;
+    let mut counter = Counter::try_from_slice(&account.data.borrow())?; // 10
 
-impl SubAssign<u32> for Counter {
-    fn sub_assign(&mut self, rhs: u32) {
-        self.count -= rhs;
-    }
-}
-
-entrypoint!(counter_contract);
-
-pub fn counter_contract(
-    //whoever is calling your program will give you three things :
-
-    // Program Id tells where the program is actually deployed.
-    program_id: &Pubkey,
-
-    // array of accounts
-    account: &[AccountInfo],
-
-    // array of u8 ( instruction)
-    instruction_data: &[u8],
-) -> ProgramResult {
-    let mut account = next_account_info(&mut account.iter())?;
-
-    // the program wants a specific format of data , so we are deserialing the instruction to match
-    // it with the Instruction I will be giving.
-    let instruction_type = InstructionType::try_from_slice(instruction_data)?;
-
-    let mut counter_data = Counter::try_from_slice(&account.data.borrow())?;
-
-    match instruction_type {
-        InstructionType::Increment(value) => {
-            counter_data += value;
+    match CounterInstruction::try_from_slice(instruction_data)? {
+        CounterInstruction::Increment(amount) => {
+            counter.count += amount; // 11
         }
-        InstructionType::Decrement(value) => {
-            counter_data -= value;
+        CounterInstruction::Decrement(amount) => {
+            counter.count -= amount;
         }
     }
 
-    counter_data.serialize(&mut *account.data.borrow_mut())?;
-    msg!("Counter updated to ,{}", counter_data);
+    counter.serialize(&mut *account.data.borrow_mut())?;
+
+    msg!("Counter updated to {}", counter.count);
 
     Ok(())
 }
